1c3429595f03ecd3956bdd22b3660c72063e7090
Olesya Miller <olesyam.miller@gmail.com>
2021-08-15 20:06:18 -0400

    created createItemDto, started working on the items controller

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Req, Res } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { Request, Response } from '@nestjs/common';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    @Get()
+    findAll(@Req() req: Request, @Res() res: Response): string {
+        return 'Get all items'
+    }
+
+    @Post()
+    create(@Body() createItemDto: CreateItemDto): string {
+        return `Name: ${createItemDto.name}, Desc: ${createItemDto.description}`
+    }
+}
